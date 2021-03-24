a30f911d3d0152fcfb6be2c268216a6ec449505b
Marshal Funk <mcfunk@uw.edu>
2021-03-24 03:00:46 -0700

    Changing db connection to on-demand eval

--- script/python/connection.py
+++ script/python/connection.py
@@ -5,12 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 connections = { 
-        'ESIT_COPY': pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
-                      'SERVER=delolydb12021;'
-                      'DATABASE=ESITDMS_COPY;'
-                      f"UID={os.getenv('ESIT_COPY_USER')};"
-                      f"PWD={os.getenv('ESIT_COPY_PASSWORD')}")
+        'ESIT_COPY': f"pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};"
+                      + 'SERVER=delolydb12021;'
+                      + 'DATABASE=ESITDMS_COPY;'
+                      + f"UID={os.getenv('ESIT_COPY_USER')};"
+                      + f"PWD={os.getenv('ESIT_COPY_PASSWORD')}")
 }
 
 def get_connection(dbname):
-    return connections[dbname]
+    return eval(connections[dbname])
